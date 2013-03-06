fc7e93fe1eabdfd6383f3ffc9a9b2e3c24655e84
targoo <targoo@gmail.com>
2013-03-06 20:20:22 +0000

    clean up add memcached

--- sites/all/themes/poly/templates/region--resume.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php if ($is_front): ?>
-
-<div id="resume">
-<?php print ($content); ?>
-</div>
-
-
-<?php endif; ?>
-	
--- sites/all/themes/poly/templates/section--footer.tpl.php
+++
@@ -1,8 +0,0 @@
-<footer<?php print $attributes; ?>>
-  <?php print $content; ?>
-  <div class="zone-wrapper zone-copyright-wrapper clearfix" id="zone-copyright-wrapper">
-  <div class="zone zone-copyright clearfix container-12" id="zone-copyright">
-  &copy; <?php print date('Y')?> Polyclinique Borinage. Powered By <a href='www.mariquecalcus.com'>MARIQUECALCUS</a>
-  </div>
-  </div>
-</footer>
